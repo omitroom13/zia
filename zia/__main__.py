036314399be51b3511e5a633ad46974381678baa
omitroom13 <22601789+omitroom13@users.noreply.github.com>
2020-10-17 19:59:53 +0900

    __main__ is cli top command

--- zia/__main__.py
+++ zia/__main__.py
@@ -1,29 +1,24 @@
-import fire
-from zia import *
+# from .defaults import load_config, get_config, RequestError, SessionTimeoutError, ZiaApiBase
+# from .session import Session
 
-class ZscalerInternetAccess(object):
-    def __init__(self, profile='default'):
-        self._session = Session(profile=profile)
-        # self.activation = Activation(self.session)
-        # self.admin_audit_logs = AdminAuditLogs(self.session)
-        # self.location = Locations(self.session)
-        # self.security = Security(self.session)
-        # self.datacenters = Datacenters(self.session)
-        # self.sandbox = Sandbox(self.session)
-        # self.ssl = Ssl(self.session)
-        # self.user = User(self.session)
-        # self.gre = Gre(self.session)
-        # self.vpn_credentials = VpnCredentials(self.session)
-        self.policies = UrlFilteringPolicies(self._session, 'str')
-        self.categories = UrlCategories(self._session, 'str')
-    def authenticate(self):
-        self._session.authenticate()
+# from zia import ZscalerInternetAccess
+import logging
+
+import fire
+
+from .defaults import load_config, get_config, RequestError, SessionTimeoutError, ZiaApiBase
+from .session import Session
+from zia import ZscalerInternetAccess
 
 def main():
+    load_config()
+    LOGGER = logging.getLogger(__name__)
+    LOGGER.setLevel(logging.DEBUG)
     z = ZscalerInternetAccess()
     z.authenticate()
     fire.Fire(z)
     return 0
+
 
 if __name__ == "__main__":
     main()
---
+++ zia/zscaler_internet_access.py
@@ -0,0 +1,33 @@
+from .defaults import load_config, get_config, RequestError, SessionTimeoutError, ZiaApiBase
+from .session import Session
+
+from .activation import Activation
+from .admin_audit_logs import AdminAuditLogs
+from .datacenters import Datacenters
+from .gre import Gre
+from .locations import Locations
+from .security import Security
+from .sandbox import Sandbox
+from .ssl import Ssl
+from .user import User
+from .vpn_credentials import VpnCredentials
+from .url_filtering_policies import UrlFilteringPolicies
+from .url_categories import UrlCategories
+
+class ZscalerInternetAccess(object):
+    def __init__(self, profile='default'):
+        self._session = Session(profile=profile)
+        self.activation = Activation(self._session, 'str')
+        self.admin_audit_logs = AdminAuditLogs(self._session, 'str')
+        # self.location = Locations(self.session)
+        # self.security = Security(self.session)
+        # self.datacenters = Datacenters(self.session)
+        # self.sandbox = Sandbox(self.session)
+        # self.ssl = Ssl(self.session)
+        # self.user = User(self.session)
+        # self.gre = Gre(self.session)
+        # self.vpn_credentials = VpnCredentials(self.session)
+        self.policies = UrlFilteringPolicies(self._session, 'str')
+        self.categories = UrlCategories(self._session, 'str')
+    def authenticate(self):
+        self._session.authenticate()
