59461f2fac8b8b6bbc8cb50fad27637b8eb6fa8a
omitroom13 <22601789+omitroom13@users.noreply.github.com>
2020-10-18 12:08:48 +0900

    implement user_management

--- zia/user_management.py
+++ zia/user_management.py
@@ -1,29 +1,73 @@
 import logging
 
-from .defaults import *
-from zia import ZiaApiBase
+from .defaults import load_config, get_config, RequestError, SessionTimeoutError, ZiaApiBase
 
-class User(ZiaApiBase):
-    def get_departments(self):
-        raise NotImplementedError()
-    def get_departments_by_id(self, department_id):
-        raise NotImplementedError()
-    def get_groups(self):
-        raise NotImplementedError()
-    def get_group_by_id(self, group_id):
-        raise NotImplementedError()
-    def get_users(self):
-        raise NotImplementedError()
-    def get_user_by_id(self, user_id):
-        raise NotImplementedError()
-    def create_user(self, user):
-        raise NotImplementedError()
-    def delete_users(self, users):
-        raise NotImplementedError()
-    def update_user(self, user):
-        raise NotImplementedError()
-    def delete_user_by_id(self, user):
-        raise NotImplementedError()
+class Departments(ZiaApiBase):
+    def list(self):
+        """
+        Gets a list of departments
+        """
+        path="departments"
+        return self._output(self._session.get(path))
+    def show(self, department_id):
+        """
+        Gets the department for the specified ID
+        """
+        path="departments/{}".format(department_id)
+        return self._output(self._session.get(path))
+
+class Groups(ZiaApiBase):
+    def list(self):
+        """
+        Gets a list of groups
+        """
+        path="groups"
+        return self._output(self._session.get(path))
+    def show(self, group_id):
+        """
+        Gets the group for the specified ID
+        """
+        path="groups/{}".format(group_id)
+        return self._output(self._session.get(path))
+
+class Users(ZiaApiBase):
+    def list(self):
+        """
+        Gets a list of all users and allows user filtering by name, department, or group
+        """
+        path="users"
+        return self._output(self._session.get(path))
+    def show(self, user_id):
+        """
+        Gets the user information for the specified ID
+        """
+        path="users/{}".format(user_id)
+        return self._output(self._session.get(path))
+    def create(self, user):
+        """
+        Adds a new user
+        """
+        path="users"
+        return self._output(self._session.post(path, user))
+    def update(self, user_id, user):
+        """
+        Updates the user information for the specified ID
+        """
+        path="users/{}".format(user_id)
+        return self._output(self._session.put(path, user))
+    def delete(self, user_object):
+        """
+        user_id : Deletes the user for the specified ID
+        user_ids : Bulk delete users up to a maximum of 500 users per request
+        """
+        t = type(user_object)
+        if t is int or t is str:
+            path="users/{}".format(user_object)
+            return self._output(self._session.delete(path))
+        elif t is dict:
+            path="users/bulkDelete"
+            return self._output(self._session.post(path, user_object))
+        raise RuntimeError('unknown user_object type {}'.format(t.__name__))
 
 
 LOGGER = logging.getLogger(__name__)
